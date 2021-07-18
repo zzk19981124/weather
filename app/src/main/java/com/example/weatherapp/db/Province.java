b84a9953e3f90185cab9cea10459445b9f506037
zzk <479432115@qq.com>
2021-07-19 00:59:56 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/weatherapp/db/Province.java
@@ -0,0 +1,33 @@
+package com.example.weatherapp.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int ProvinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return ProvinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        ProvinceCode = provinceCode;
+    }
+}
